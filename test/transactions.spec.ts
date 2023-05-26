c7cb90e3607d9c6bf0c286305e93a3d33f3dbe63
Gabriel52 <gabrielbrune52@gmail.com>
2023-05-25 23:26:30 -0300

    test<transactions>: add test to list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, expect, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/server'
 
@@ -12,12 +12,34 @@ describe('Transactions routes', () => {
   })
 
   it('should be able to create a transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New HD',
-      amount: 400,
-      type: 'debit',
-    })
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New HD',
+        amount: 400,
+        type: 'debit',
+      })
+      .expect(201)
+  })
 
-    expect(response.statusCode).to.equal(201)
+  it('should be able see all transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New HD',
+        amount: 400,
+        type: 'credit',
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New HD',
+        amount: 400,
+      }),
+    ])
   })
 })
